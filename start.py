e3a200da54a7cb94c51551e8917933026ab20a18
fzangouie <fzangooie@yahoo.com>
2015-12-30 12:22:14 +0330

    f1

--- start.py
+++ start.py
@@ -7,7 +7,8 @@ import tornado.web
 from tornado.options import define, options
 from urls import urlList
 
-define("port", default=8090, help="run on the given port", type=int)
+
+define("port", default=4000, help="run on the given port", type=int)
 
 class EducationPortal(tornado.web.Application):
 
