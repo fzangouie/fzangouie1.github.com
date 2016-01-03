3b506b4d0e70ed5a3972536afe4daa5b58f857b3
fzangouie <fzangooie@yahoo.com>
2016-01-03 11:05:26 +0330

    f3

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ __author__ = 'user'
 
 class Config:
     def __init__(self):
-        pass
+        static_path = os.path.join(os.path.dirname(__file__), "static")
 
-    static_path = os.path.join(os.path.dirname(__file__), "static")
